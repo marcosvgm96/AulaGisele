12586c828896df3b57adc67985e48ba6576fcf4b
markitoguerreiro <marcos.vgm@hotmail.com>
2020-08-12 23:29:31 -0300

    AtividadeGisele

--- Arquivo1.py
+++ Arquivo1.py
@@ -4,12 +4,11 @@ class Maiordeidade():
 
     def testeidade(self, idade):
         if idade >= 18:
-            print("essa pessoa é maior de idade!")
+            print("Essa pessoa é maior de idade!")
         else:
-            print("essa pessoa é menor de idade!")
+            print("Essa pessoa é menor de idade!")
         return idade
-
 
 pessoa = Maiordeidade()
 pessoa.metodo1()
-pessoa.testeidade(int(input("qual a sua idade: ")))
+pessoa.testeidade(int(input("Sua idade: ")))
